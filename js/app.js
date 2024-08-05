eea82265550c5560b00fc655d197dbfb2b3ed2c8
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2024-08-05 10:28:06 +0300

    add

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-const swiper = new Swiper('.swiper', {
-     mousewheel: true,
-     allowTouchMove: true,
-     direction: 'vertical',
-     speed: 1700,
-     parallax: true
+import Swiper, { Mousewheel } from 'swiper';
+Swiper.use([Mousewheel]);
 
+const swiper = new Swiper('.swiper', {
+      direction: 'vertical',
+      speed: 1700,
+      parallax: true,
+      mousewheel: {
+        invert: false, // Измените это значение, если необходимо
+        releaseOnEdges: true, // Параметр для активации прокрутки на краях
+      },
+      allowTouchMove: true,
   });
 
   swiper.on('slideChange', function() {
@@ -12,3 +17,11 @@ const swiper = new Swiper('.swiper', {
          return swiper.activeIndex === i ? e.classList.add('active') : e.classList.remove('active')
    })
   })
+
+  swiper.on('init', function () {
+      console.log('Swiper initialized');
+    });
+    
+    swiper.on('slideChange', function () {
+      console.log('Slide changed');
+    });
