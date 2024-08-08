e6ddc90d8df9ccd2b3672bf6796a17407e9164ae
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2024-08-08 12:59:03 +0300

    fix bug in html

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 const swiper = new Swiper('.swiper', {
-	mousewheel: true,
-	direction: 'vertical',
-	speed: 1700,
-	parallax: true
-})
+      direction: 'vertical',
+      speed: 1700,
+      parallax: true,
+      scrollbar: {
+          el: '.swiper-scrollbar',
+          draggable: true,
+      },
+      mousewheel: {
+          invert: false,
+      },
+      // Убедитесь, что touch управление включено
+      touch: {
+          enabled: true,
+      },
+  });
 
   swiper.on('slideChange', function() {
    document.querySelectorAll('.header-content__slide').forEach(function(e, i) {
          return swiper.activeIndex === i ? e.classList.add('active') : e.classList.remove('active')
    })
-  })
+  });
 
   swiper.on('init', function () {
       console.log('Swiper initialized');
